90252006305cde1c27d79f6a4940548cc413c585
Jakub Hesoun <jakub@hesoun.com>
2017-08-14 20:21:39 +0200

    initial commit

---
+++ src/main/java/com/hesoun/yahoo/YahooObject.java
@@ -0,0 +1,16 @@
+package com.hesoun.yahoo;
+
+/**
+ * @author Jakub Hesoun
+ */
+public class YahooObject {
+    private Chart chart;
+
+    public Chart getChart() {
+        return chart;
+    }
+
+    public void setChart(Chart chart) {
+        this.chart = chart;
+    }
+}
